5d4573ca9c4083c912c98e63cf49c13fba1fa4fd
Olha Gruzglina <olga.zhmura@gmail.com>
2022-02-01 13:41:56 -0500

    commit tree 69eadcdd557a0de5fc62e832ed525b7dbada4c24

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,20 @@
 // Code Keypad Component Here
+import React from 'react';
+
+function handleChange () {
+    console.log('Entering password...');
+}
+
+function Keypad () {
+    function handleBlur (event) {
+        console.log(event);
+        event.target.form.reset(); //reset the input field when it's not in focus
+    }
+    return (
+        <form>
+            <input type="password" placeholder="Type your password" onChange = {handleChange} onBlur={handleBlur}/>
+        </form>
+    );
+}
+
+export default Keypad;
